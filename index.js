6626ca8b9a74c0475a4d4febfaca8364d2731de1
Jay <jay@Jay.local>
2017-06-16 22:21:39 -0400

    Fix 3rd syntax error

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/',function(req,res){
 //Facebook
 
 app.get('/webhook/',function(req,res){
-    if(req.query['hub.verify_token']==="richman"){
+    if(req.query['hub.verify_token']=="richman"){
         res.send(req.query['hub.challenge'])
     }
     res.send("Wrong token")
